a41639cc82698f9e4ed3b88456102babc601c747
mrabusalah <abdullah.adel@progressoft.com>
2020-10-08 23:43:45 +0300

    feat: adding authorization classes

---
+++ src/main/java/com/bau/graduateprojects/qrstudentsattendance/auth0/JwtUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.bau.graduateprojects.qrstudentsattendance.auth0;
+
+
+import com.bau.graduateprojects.qrstudentsattendance.models.Student;
+import com.bau.graduateprojects.qrstudentsattendance.repositories.StudentRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+@Service("StudentService")
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final StudentRepository studentRepository;
+
+    public JwtUserDetailsService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Student student = studentRepository.findStudentByUniversityId(username);
+        if (student == null) {
+            throw new UsernameNotFoundException("Student not found with username: " + username);
+        }
+
+        return student;
+    }
+
+}
--- src/main/java/com/bau/graduateprojects/qrstudentsattendance/models/Student.java
+++ src/main/java/com/bau/graduateprojects/qrstudentsattendance/models/Student.java
@@ -1,28 +1,66 @@
 package com.bau.graduateprojects.qrstudentsattendance.models;
 
-import com.bau.graduateprojects.qrstudentsattendance.services.DateTimeService;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 @Data
-public class Student {
+@Entity
+public class Student implements UserDetails {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+    private String email;
     private String universityId;
+    private String password;
     private LocalDate birthDay;
     private int numberOfAbsence;
     private LocalDateTime creationDate;
 
     @PrePersist
     public void prePersist() {
-        this.creationDate = DateTimeService.currentDateTime();
+        this.creationDate = LocalDateTime.now();
         this.numberOfAbsence = 0;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
