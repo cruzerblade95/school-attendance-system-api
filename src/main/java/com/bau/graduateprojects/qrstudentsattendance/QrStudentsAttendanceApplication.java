c5438e6eff9174467656edf138d301739fa1134c
mrabusalah <abdullah.adel@progressoft.com>
2020-10-08 20:53:45 +0300

    First commit

---
+++ src/main/java/com/bau/graduateprojects/qrstudentsattendance/QrStudentsAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.bau.graduateprojects.qrstudentsattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrStudentsAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrStudentsAttendanceApplication.class, args);
+	}
+
+}
