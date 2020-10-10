1d9adab455d831c1854926ac4e4cb50f14b70129
mrabusalah <abdullah.adel@progressoft.com>
2020-10-11 00:25:53 +0300

    feat: adding test for student service

--- src/main/java/com/bau/graduateprojects/qrstudentsattendance/services/StudentService.java
+++ src/main/java/com/bau/graduateprojects/qrstudentsattendance/services/StudentService.java
@@ -5,6 +5,8 @@ import com.bau.graduateprojects.qrstudentsattendance.repositories.StudentReposit
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import static java.util.Objects.isNull;
+
 @Service
 public class StudentService {
 
@@ -15,6 +17,7 @@ public class StudentService {
     }
 
     public Student createStudent(Student student) {
+        throwIfNull(student, "student");
         encodePassword(student);
         return studentRepository.save(student);
     }
@@ -23,5 +26,10 @@ public class StudentService {
         String encodedPassword = new BCryptPasswordEncoder()
                 .encode(student.getPassword());
         student.setPassword(encodedPassword);
+    }
+
+    private void throwIfNull(Object o, String fieldName) {
+        if (isNull(o))
+            throw new IllegalArgumentException(fieldName + " must not be null");
     }
 }
---
+++ src/test/java/com/bau/graduateprojects/qrstudentsattendance/services/StudentServiceTest.java
@@ -0,0 +1,40 @@
+package com.bau.graduateprojects.qrstudentsattendance.services;
+
+import com.bau.graduateprojects.qrstudentsattendance.models.Student;
+import com.bau.graduateprojects.qrstudentsattendance.repositories.MockStudentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class StudentServiceTest {
+
+    private StudentService studentService;
+
+    @BeforeEach
+    void setUp() {
+        MockStudentRepository mockStudentRepository = new MockStudentRepository();
+        studentService = new StudentService(mockStudentRepository);
+    }
+
+    @Test
+    void givenNullStudentWhenCreateStudentThenFail() {
+        Student student = null;
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> studentService.createStudent(student));
+        assertEquals("student must not be null", exception.getMessage());
+    }
+
+    Student makeStudent() {
+        Student student = new Student();
+        student.setUsername("username");
+        student.setPassword("password");
+        student.setName("name");
+        student.setBirthDay(LocalDate.now());
+        student.setEmail("email");
+        return student;
+    }
+}
