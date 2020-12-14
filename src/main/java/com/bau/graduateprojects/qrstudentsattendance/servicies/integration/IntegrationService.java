3d3756401615222a3671254e34d00ff8f2e9f9cd
mrabusalah <abdullah.adel@progressoft.com>
2020-12-14 22:56:28 +0200

    feat: adding integration end point between entities

---
+++ src/main/java/com/bau/graduateprojects/qrstudentsattendance/servicies/integration/IntegrationService.java
@@ -0,0 +1,14 @@
+package com.bau.graduateprojects.qrstudentsattendance.servicies.integration;
+
+import com.bau.graduateprojects.qrstudentsattendance.entities.StudentEntity;
+import com.bau.graduateprojects.qrstudentsattendance.entities.TeacherEntity;
+
+public interface IntegrationService {
+    StudentEntity addCourseToStudent(Long sId, Long cId);
+
+    void removeCourseFromStudent(Long sId, Long cId);
+
+    TeacherEntity addCourseToTeacher(Long tId, Long cId);
+
+    void removeCourseToTeacher(Long tId, Long cId);
+}
